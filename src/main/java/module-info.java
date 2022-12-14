0d0d44bd7bd81bb7bc831e05fac5802e7f98bbc4
ashutosh2304 <92710679+ashutosh2304@users.noreply.github.com>
2022-12-14 10:58:11 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp1011200476114test2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp1011200476114test2 to javafx.fxml;
+    exports com.example.comp1011200476114test2;
+}
